38d655678384565cee4894941c367248fbc1242a
CaoBin <346322003@qq.com>
2017-06-10 21:04:32 +0800

    添加rabbitmq

--- base/runDB.sh
+++ base/runDB.sh
@@ -11,4 +11,5 @@ docker run -d --network test --ip 172.18.0.200  -p 3306:3306 --hostname mysql --
 docker run -d --network test --ip 172.18.0.201  -p 6379:6379 --hostname redis --name redis redis
 docker run -d --network test --ip 172.18.0.202  -p 27017:27017 --hostname mongo --name mongo mongo
 docker run -d --network test --ip 172.18.0.203  -p 9092:9092 --hostname kafka --name kafka registry.cn-hangzhou.aliyuncs.com/wyun/kafka:0.10.2.1_zk
+docker run -d --network test --ip 172.18.0.204  -p 5672:5672 -p 15672:15672 --hostname rabbit --name rabbit -e RABBITMQ_DEFAULT_VHOST=oo -e RABBITMQ_DEFAULT_USER=root -e RABBITMQ_DEFAULT_PASS=root rabbitmq:3.6.10-management
 
